e2c22f7e17628e69a7d70c6f95678dbf5f211964
Isak La Fleur <isak.engdahl@se.atlascopco.com>
2017-02-23 17:03:59 +0100

    .

--- app.js
+++ app.js
@@ -2,4 +2,15 @@ const express = require('express');
 
 const app = express();
 
+// Watch for incoming HTTP request of method GET
+// to the route http://localhost:3050/api
+app.get('/api', (req, res) => {
+    res.send({ hi: 'there' });
+});
+
+
+//app.post('/api');
+//app.put('api');
+//app.delete('/api')
+
 module.exports = app;
